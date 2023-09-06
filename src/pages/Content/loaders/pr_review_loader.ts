a75992891cff718e88bad749da1200e36daa8df2
Muhammad Numan <muhammadnuman70@gmail.com>
2023-09-06 14:56:34 +0500

    fix: edit PR and issues

--- src/pages/Content/loaders/pr_review_loader.ts
+++ src/pages/Content/loaders/pr_review_loader.ts
@@ -3,17 +3,31 @@ import { initReactAppIntoMarkdownHeader } from './common';
 
 // for PR reviews
 //@ts-ignore
-document.arrive(
-  'tab-container.js-previewable-comment-form',
-  function (newElem: Element) {
-    console.log('markdow editor have come into dom');
-    RecentEmojis.init();
-    const tabHeader = newElem.querySelector('div.tabnav-tabs[role="tablist"]');
-    const textArea: HTMLTextAreaElement | null = newElem.querySelector(
-      'textarea[name="comment[body]"]'
+document.arrive('.js-previewable-comment-form', function (newElem: Element) {
+  console.log('markdow editor have come into dom');
+  RecentEmojis.init();
+  const tabHeader = newElem.querySelector('.tabnav-tabs[role="tablist"]');
+  const textArea: HTMLTextAreaElement | null = newElem.querySelector(
+    'textarea[name="comment[body]"]'
+  );
+
+  if (tabHeader && textArea) {
+    initReactAppIntoMarkdownHeader(tabHeader, textArea);
+  } else {
+    // for open pull request arrive
+    const textArea1: HTMLTextAreaElement | null = newElem.querySelector(
+      'textarea[id="pull_request_body"]'
     );
-    if (tabHeader && textArea) {
-      initReactAppIntoMarkdownHeader(tabHeader, textArea);
+    if (tabHeader && textArea1) {
+      initReactAppIntoMarkdownHeader(tabHeader, textArea1);
+    } else {
+      // for edit pull request main textarea
+      const textArea2: HTMLTextAreaElement | null = newElem.querySelector(
+        'textarea[name="pull_request[body]"]'
+      );
+      if (tabHeader && textArea2) {
+        initReactAppIntoMarkdownHeader(tabHeader, textArea2);
+      }
     }
   }
-);
+});
